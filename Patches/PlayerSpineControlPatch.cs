9e9428606f8a8af7a4567bc41ef87ac52f6bfc5c
tbv001 <sebuahakun123@gmail.com>
2026-07-24 17:29:27 +0700

    Fix flipping spine deviation outside first person

--- Patches/PlayerSpineControlPatch.cs
+++ Patches/PlayerSpineControlPatch.cs
@@ -10,7 +10,8 @@ internal static class PlayerSpineControlPatch
     private static void FixSpine(PlayerSpineControl __instance, ref float ___deviationY)
     {
         var playerMain = __instance.GetComponentInParent<PlayerMain>();
-        if (playerMain != null && !playerMain.ForeignPlayer && ViewmodelOffset.Flip.Value)
+        if (playerMain != null && !playerMain.ForeignPlayer &&
+            PlayerCamera.instance?.CurrentMode == PlayerCamera.Mode.FirstPerson && ViewmodelOffset.Flip.Value)
         {
             ___deviationY = -___deviationY;
         }
